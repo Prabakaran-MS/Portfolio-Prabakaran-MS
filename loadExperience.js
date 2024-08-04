3ae9fc7dda8b542c7bae472e4486e62c43b91a9c
Prabakaran M S <123766512+Prabakaran-MS@users.noreply.github.com>
2024-08-05 00:28:21 +0530

    Work Experience Updated.

--- loadExperience.js
+++ loadExperience.js
@@ -10,8 +10,12 @@ function loadPersonalInfo(data) {
                         <i class="bi bi-calendar me-2"></i>${data.period.join(" - ")}
                     </div>
                     <div class="col-md-9">
-                        <h4>${data.Organisation} <small><i class="bi bi-arrow-up-right"></i></small></h4>
-                        <p>${data.desigination} </p>
+                        <h4>
+                            <img src="${data.logo}" alt="${data.Organisation} logo" class="me-2" style="width: 40px; height: 40px;">
+                            ${data.Organisation} <small><i class="bi bi-arrow-up-right"></i></small>
+                        </h4>
+                        <p>${data.designation}</p>
+                        <p>${data.location}</p>
                         <p>${data.workDetail}</p>
                         <div class="technology d-flex flex-wrap">
                         ${data.techUsed.map(tech => `<div class="rounded-pill text-bg-primary p-2 m-1">${tech}</div>`).join('')}
@@ -22,12 +26,16 @@ function loadPersonalInfo(data) {
     experienceContainer.appendChild(div);
 }
 
-const mphasis = {
-    Organisation: "Mphasis",
-    desigination: "Software Engineer",
-    period: ["2024", "PRESENT"],
-    workDetail: "Develop high-quality production code for diverse client projects, including prestigious institutions like Harvard Business School, Vanderbilt University, and more. Provide leadership and mentorship within the engineering department.",
-    techUsed: ['JavaScript', 'HTML', 'CSS', 'Bootstrap']
-}
 
-loadPersonalInfo(mphasis);
+const advantest = {
+    Organisation: "Advantest",
+    designation: "Junior Software Engineer",
+    location: "Chennai, India",
+    logo: "./images/organization/Advantest.png", 
+    period: ["Jan 2023", "Present"],
+    workDetail: "Developed high-quality production code for diverse client projects. Worked on server projects and driverSets, including adding features, refactoring code, and creating documentation. Enhanced company software product suite with focus on performance improvements and bug fixes. Automated deployment processes by creating batch scripts for publish folder generation and managed shell scripts for various tasks. Conducted manual testing to ensure software quality and performance.",
+    techUsed: ['C#', 'Winforms', 'Shell Script', 'Batch Script']
+};
+
+// Load personal info for both Mphasis and Advantest
+loadPersonalInfo(advantest);
